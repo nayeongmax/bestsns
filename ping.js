ec34447077ea1155205735a7e7a3b7cff0fffaa2
nayeongmax <nayeong6542@gmail.com>
2026-02-13 10:42:38 +0900

    Delete ping.js

--- ping.js
+++
@@ -1,6 +0,0 @@
-// 테스트용 함수 - Netlify Functions 배포 확인용
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ ok: true, message: 'Functions 동작 중' }),
-});
